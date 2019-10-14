bd876060bb23ae21adf6f7d4fa1683c5ac1ef7bc
Martin Ratinaud <martinratinaud@gmail.com>
2019-10-14 18:27:11 +0400

    chore: install prettier

--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -2,11 +2,7 @@ import * as React from 'react';
 import './styles/index.css';
 
 const App = () => {
-  return (
-    <div>
-      APP
-    </div>
-  );
-}
+  return <div>APP</div>;
+};
 
 export default App;
