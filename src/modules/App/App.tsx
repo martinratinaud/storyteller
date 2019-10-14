2b64ed67d37bf9607de2ee9b78811320f856dd61
Martin Ratinaud <martinratinaud@gmail.com>
2019-10-14 18:37:48 +0400

    feat: add new homepage with styled-components

--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import './styles/index.css';
 
+const Layout = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
 const App = () => {
-  return <div>APP</div>;
+  return <Layout>APP</Layout>;
 };
 
 export default App;
