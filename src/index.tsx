a4dee8e25820448d5707eb7261df7c49b90686b1
Martin Ratinaud <martinratinaud@gmail.com>
2019-10-14 18:24:25 +0400

    chore: adapt folder structure to meet module style and remove unused files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './modules/App/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/modules/App/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './styles/index.css';
+
+const App = () => {
+  return (
+    <div>
+      APP
+    </div>
+  );
+}
+
+export default App;
